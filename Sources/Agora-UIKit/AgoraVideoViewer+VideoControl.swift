b7041fc6d0dcd5c674d6dd137c74859ccab2f2cc
Max Cobb <maxxc@mac.com>
2021-09-22 16:14:49 +0100

    added settings to change videoSource in 4.x SDK

--- Sources/Agora-UIKit/AgoraVideoViewer+VideoControl.swift
+++ Sources/Agora-UIKit/AgoraVideoViewer+VideoControl.swift
@@ -17,9 +17,12 @@ extension AgoraVideoViewer {
             return
         }
         self.getControlContainer()
-        if let videoSource = self.agSettings.videoSource {
-            self.agkit.setVideoSource(videoSource)
-        }
+
+        self.agkit.setExternalVideoSource(
+            agSettings.externalVideoSource.enabled,
+            useTexture: agSettings.externalVideoSource.texture,
+            encodedFrame: agSettings.externalVideoSource.encoded
+        )
         if self.agSettings.externalAudioSettings.enabled {
             let audioSource = self.agSettings.externalAudioSettings
             self.agkit.enableExternalAudioSource(
@@ -27,7 +30,6 @@ extension AgoraVideoViewer {
                 channelsPerFrame: .init(audioSource.channels)
             )
         }
-
         self.agkit.setVideoEncoderConfiguration(self.agoraSettings.videoConfiguration)
     }
 
