c1c584bddfaa792209fa94ab22aca6e13a19abee
Max Cobb <maxxc@mac.com>
2021-09-27 15:04:33 +0100

    add externalAudioSource to AgoraSettings

--- Sources/Agora-UIKit/AgoraVideoViewer+VideoControl.swift
+++ Sources/Agora-UIKit/AgoraVideoViewer+VideoControl.swift
@@ -20,6 +20,14 @@ extension AgoraVideoViewer {
         if let videoSource = self.agSettings.videoSource {
             self.agkit.setVideoSource(videoSource)
         }
+        if self.agSettings.externalAudioSource.enabled {
+            let audioSource = self.agSettings.externalAudioSource
+            self.agkit.enableExternalAudioSource(
+                withSampleRate: .init(audioSource.sampleRate),
+                channelsPerFrame: .init(audioSource.channels)
+            )
+        }
+
         self.agkit.setVideoEncoderConfiguration(self.agoraSettings.videoConfiguration)
     }
 
